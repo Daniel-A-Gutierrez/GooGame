5a37af620f4f57f4b64b8275b2a106dc495fac0e
khuang0312 <38226080+khuang0312@users.noreply.github.com>
2019-10-20 15:47:47 -0700

    Assets for the menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Credits;
+    public GameObject Instructions;
+
+    public AudioClip clip;
+    private AudioSource source;
+
+    private void Start()
+    {
+        MainMenu.SetActive(true);
+        //source = GetComponent<AudioSource>();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1"); 
+    }
+
+    public void EnterInstructions()
+    {
+        MainMenu.SetActive(false);
+        Instructions.SetActive(true);
+    }
+
+    public void EnterCredits()
+    {
+        MainMenu.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    public void ExitInstructions()
+    {
+        MainMenu.SetActive(true);
+        Instructions.SetActive(false);
+    }
+
+    public void ExitCredits()
+    {
+        MainMenu.SetActive(true);
+        Instructions.SetActive(false);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
