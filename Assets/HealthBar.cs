08a80fa65ed63708b07948d1ce1df364e9af84d0
khuang0312 <38226080+khuang0312@users.noreply.github.com>
2019-10-20 13:37:15 -0700

    Live Counter

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    
-    
 
-   
-    
+    public int lives; //for testing purposes
 
+    GameObject canvas;
+    Image image;
+
+    public Sprite[] sprite = new Sprite[4];
 
 
 
@@ -17,12 +19,38 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        image = GetComponent<Image>();
+        image.sprite = sprite[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LiveCountUpdate();
+    }
+
+    void LiveCountSetter(int i)
+    {
+        if (lives + i >= 0 || lives + i <= 3)
+        {
+            lives += i;
+        }
+        image.sprite = sprite[lives];
+    }
+
+
+    void LiveCountUpdate()
+    {
+        if (lives > 3)
+        {
+            lives = 3;
+        }
+
+        else if (lives < 0)
+        {
+            lives = 0;
+        }
+        image.sprite = sprite[lives];
     }
 }
