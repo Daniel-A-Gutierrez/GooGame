06b0e073832c53b09a4a15437f25876377adb4cd
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-10-20 13:07:30 -0700

    Reset Button

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResetButton : MonoBehaviour
+{
+    public KeyCode resetButton;
+    void Update()
+    {
+        if(Input.GetKeyDown(resetButton))
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+}
