461ce5e124b00f890aaa04abcaf4e43c253dacf5
Conan Truong <truong.conan@gmail.com>
2019-10-20 17:27:58 -0700

    Level 3

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] CameraFollow cameraFollow;
+    [SerializeField] MainPC mainPC;
+
+    bool activated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!activated)
+        {
+            activated = true;
+            mainPC.enabled = false;
+            cameraFollow.enabled = false;
+            SceneTransition.instance.StartTransition(SceneManager.GetActiveScene().name, 30);
+        }
+    }
+}
